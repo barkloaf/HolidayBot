ed37be2b28c2f83fc27f67ce015ebbb75a37b11b
barkloaf <46911272+barkloaf@users.noreply.github.com>
2019-01-21 20:47:02 -0700

    Revert "Actual adding of the bot files"
    This reverts commit a9fdab659a8568c4bfd0d4d0e661ce0825cf467c.

--- events/guildDelete.js
+++
@@ -1,9 +0,0 @@
-let clc = require("cli-color")
-let moment = require("moment")
-require("moment-timezone");
-require("moment-duration-format");
-
-module.exports.run = async (client, guild) => {
-    console.log("[" + clc.blue("INFO") + "] " + `Removed from guild: "${guild.name}" (ID: ${guild.id})`);
-    client.cmdHook.send("`[" + `${moment().format('DD/MM/YYYY] [HH:mm:ss')}` + "]`" + "[**" + "INFO" + "**] " + `Removed from guild: \`${guild.name}\` (ID: ${guild.id})`)
-}
